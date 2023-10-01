fd5babd74254ee71b2fdfb11bf4d8d9d43f5f501
nmdra <nimendradilshan11@gmail.com>
2023-10-01 15:46:39 +0530

    Convert to php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservify</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<?php include "./partials/header.php" ?>
+<body>
+
+    <div class="main">
+        <!-- Your main content goes here -->
+    </div>
+</body>
+
+<?php include "./partials/footer.php" ?>
+
+</html>
+
---
+++ partials/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservify</title>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<!-- Navigation Bar -->
+<nav class="navBar">
+    <h1 class="site-logo"><a href="../index.php">Reservify</a></h1>
+    <ul>
+        <li><a href="../about.php">About Us</a></li>
+        <li><a href="../hotel.php">Hotel</a></li>
+        <li><a href="../contact.php">Contact Us</a></li>
+    </ul>
+
+    <div class="btn">
+        <button class="loginBtn"><a href="../login.php">Login</a></button>
+        <button class="signBtn"><a href="../register.php">Register</a></button>
+    </div>
+</nav>
+
+</html>
