a049036a632e70352ae5d3c9ea5f607e8a994abd
NIMENDRA <73674803+nmdra@users.noreply.github.com>
2023-10-05 01:33:03 +0530

    Add Logout option (#9)
    * Add Workflow
    
    * Update CSS Color
    
    * Create Login And Sign Up
    
    * Add reservify database
    
    * remove database
    
    * Add Logout Button

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,6 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservify</title>
     <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <!-- Navigation Bar -->
@@ -16,10 +21,21 @@
         <li><a href="../hotel.php">Hotel</a></li>
         <li><a href="../contact.php">Contact Us</a></li>
     </ul>
-
-    <div class="btn">
+<div class="btn">
+    <?php
+    //if there is a user display username
+    if (isset($_SESSION['username'])) {
+        echo '<div class="btn">
+        <button class="username"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></g></svg>&nbsp;&nbsp;<a href="./userDashboard.php">';
+        echo $_SESSION['name'] . '</a><i class="fas fa-user-check"></i></button>';
+        echo '<button class="logoutbtn"><a href="./logout.php">Logout</a></button></div>';
+    } else {
+        //if the user is not a registered user display a register button
+        echo '<div class="btn">
         <button class="loginBtn"><a href="./login.php">Login</a></button>
-        <button class="signBtn"><a href="./register.php">Register</a></button>
+        <button class="signBtn"><a href="./register.php">Register</a></button>';
+    }
+    ?>
     </div>
 </nav>
 
--- userDashboard.php
+++ userDashboard.php
@@ -1,2 +1,35 @@
-<?php 
- echo "WIP";
+<?php
+
+    if (!isset($_SESSION['username'])) {
+        echo " 
+        <script>
+        alert('You Must Login to access this page');
+        window.location.href='./login.php';
+        </script>";
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reservify</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<?php include "./partials/header.php" ?>
+
+<body>
+
+    <div class="main">
+        <!-- Your main content goes here -->
+        <H1>WIP</H1>
+    </div>
+</body>
+
+<?php include "./partials/footer.php" ?>
+
+</html>
