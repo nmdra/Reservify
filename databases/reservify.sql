dbf01999aa26f3899f716dc64e4fd37c255f36f2
NIMENDRA <73674803+nmdra@users.noreply.github.com>
2023-10-08 03:18:17 +0530

    Add Hotel Page (#16)

--- databases/reservify.sql
+++ databases/reservify.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 06, 2023 at 10:04 AM
+-- Generation Time: Oct 07, 2023 at 11:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `reservify`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Hotel`
+--
+
+CREATE TABLE `Hotel` (
+  `hid` int(11) NOT NULL,
+  `hName` varchar(40) NOT NULL DEFAULT 'hotel',
+  `hDescription` text NOT NULL,
+  `imgname` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `Hotel`
+--
+
+INSERT INTO `Hotel` (`hid`, `hName`, `hDescription`, `imgname`) VALUES
+(1, 'Amora Resort', 'Experience unparalleled luxury and tranquility at Amora Resort. Nestled on the picturesque shores of Negombo, our resort offers a perfect blend of modern comfort and natural beauty. Whether you\'re looking for a relaxing beachfront getaway or an adventurous water sports experience, Amora Resort has it all. Our well-appointed rooms, delectable dining options, and attentive service ensure a memorable stay. Discover the true essence of tropical paradise at Amora Resort', 'img1.webp'),
+(2, 'Oceanview Paradise', 'Escape to Oceanview Paradise, where your dream vacation awaits. Situated along the pristine coastline of Galle, Sri Lanka, our hotel offers breathtaking views of the Indian Ocean and an oasis of serenity.Our luxurious rooms and suites are designed to provide the utmost comfort and style. Whether you\'re lounging by the infinity pool, indulging in culinary delights at our oceanfront restaurant, or enjoying a spa treatment with the sound of waves in the background, Oceanview Paradise promises an unforgettable experience.For the adventurous traveler, explore nearby historic sites, embark on a whale-watching excursion, or simply stroll along the golden sands of our private beach.At Oceanview Paradise, every moment is a journey of relaxation and discovery. Come, unwind, and create lasting memories in this coastal haven.', 'img3.jpg'),
+(3, 'Araliya Beach Resort', 'skjdljsljdlsj:', 'img4.avif'),
+(4, 'Araliya mount Resort', 'skjdljsljdlsj:', 'img2.jpg');
 
 -- --------------------------------------------------------
 
@@ -51,6 +74,12 @@ INSERT INTO `user` (`uid`, `uName`, `name`, `email`, `password`, `role`) VALUES
 --
 
 --
+-- Indexes for table `Hotel`
+--
+ALTER TABLE `Hotel`
+  ADD PRIMARY KEY (`hid`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -59,6 +88,12 @@ ALTER TABLE `user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `Hotel`
+--
+ALTER TABLE `Hotel`
+  MODIFY `hid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
--- hotel.php
+++ hotel.php
@@ -1,5 +1,56 @@
-<?php 
+<?php
+session_start()
+?>
 
-echo "WIP"
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/hotel.css">
+</head>
+
+<?php include "./partials/header.php" ?>
+
+<body>
+
+
+<div class="title">
+    <h1>HOTELS</h1>
+</div>
+
+<div class="cardContainer">
+    
+
+    <?php
+
+    require_once './conn.php';
+    // Check if the values exist in the database
+    $query = "SELECT * FROM `Hotel`";
+    $result = mysqli_query($conn, $query);
+    // $row = mysqli_fetch_array($result);
+
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row['hid'];
+            $name = $row['hName'];
+            $imgname = $row['imgname'];
+
+        echo '<div class="card">';
+        echo '<img src="./assets/image/' . $imgname .'" alt=hotel>';
+        echo '<h1>' . $name . '</h1>';
+        echo '<button onclick="location.href=\'./booking.php?hotel=' . $id . '\'" type="button">Book Now</button>';
+        echo '</div>';
+        }
+    }
 
 ?>
+</div>
+
+</body>
+
+<?php include "./partials/footer.php" ?>
+</html>
