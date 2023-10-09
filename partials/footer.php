eb3f7f7f8ca52cf8faf584c43c1ff6aff020e6f3
nmdra <nimendradilshan11@gmail.com>
2023-10-09 12:16:14 +0530

    Update Header and Footer

--- partials/footer.php
+++ partials/footer.php
@@ -2,9 +2,6 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservify</title>
     <link rel="stylesheet" href="css/footer.css">
 </head>
 
